90cb0ba4399a0428b88a0a41de0a1b46cb2c64b9
Crymnc <can.sahintas2324@gmail.com>
2020-12-13 01:47:41 +0300

    getBeerById added

--- src/main/java/com/spring/breweryclient/web/client/BreweryClient.java
+++ src/main/java/com/spring/breweryclient/web/client/BreweryClient.java
@@ -1,7 +1,12 @@
 package com.spring.breweryclient.web.client;
 
+import com.spring.breweryclient.web.model.Beer;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
 
 @Component
 //Adding property externally with application.properties
@@ -11,6 +16,15 @@ public class BreweryClient {
 
     public final String BEER_PATH_V1="api/v1/beer";
     private String apihost;
+    private final RestTemplate restTemplate;
+
+    public BreweryClient(RestTemplateBuilder restTemplateBuilder){
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    public Beer getBeerById(UUID id){
+        return restTemplate.getForObject(apihost+BEER_PATH_V1+id.toString(),Beer.class);
+    }
 
     public void setApiHost(String apiHost){
         this.apihost = apiHost;
